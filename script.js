e2b24782a4665456218cdd2b84d4431d54298d8c
Rocket457 <eduardoanibal457@gmail.com>
2023-08-28 21:52:07 -0300

    a
    a

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ btn.addEventListener("click", function(e) {
     const name = document.querySelector("#name");
     const value = name.value;
     console.log(value)
-    if(value == "carolina"|| value == "carol"){
-        
+    value.toUpperCase()
+    if(value == "CAROLINA"||value == "CAROL"){
         mylove.innerHTML = value.toUpperCase() + "!! isso voce acertou :D";
     } else{
         mylove.innerHTML = "Não o nome dela não é " + value + ", voce errou o nome dela :(";
