135b9c01190ec4e0123af4cffdf0af57d55ecaad
Rocket457 <eduardoanibal457@gmail.com>
2023-08-28 21:38:12 -0300

    tudo que a de necessario
    prontinho

---
+++ script.js
@@ -0,0 +1,19 @@
+var btn = document.querySelector("#send")
+const mylove = document.querySelector("#mylove");
+
+
+btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    const name = document.querySelector("#name");
+    const value = name.value;
+    console.log(value)
+    if(value == "carolina"){
+        
+        mylove.innerHTML = value.toUpperCase() + "!! isso voce acertou :D";
+    } else{
+        mylove.innerHTML = "Não o nome dela não é " + value + ", voce errou o nome dela :(";
+    }
+
+
+});
+
